0fc22308738c6924029631c74f5731fd1280b5d5
Viktor Grant <isahara86@gmail.com>
2023-07-02 23:03:24 +0200

    initial commit

---
+++ c_blazor_aspnet_hosted/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using c_scharp_companies.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace c_scharp_companies.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Address> Address { get; set; } = default!;
+}
+
---
+++ c_blazor_aspnet_hosted/Server/Models/Address.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace c_scharp_companies.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
